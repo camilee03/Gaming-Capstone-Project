cb3ea4ae31c0210ff7819c5ce59d19e6aac37649
CGSmoothie <sericksonbob@gmail.com>
2025-04-22 16:10:17 -0700

    Main Menu :)

---
+++ Gaming Capstone Project/Assets/CustomSessionStuff.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class CustomSessionStuff : MonoBehaviour
+{
+    public TMP_InputField inputField;
+    public Button[] buttons;
+    public void EnterSession()
+    {
+        var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        string finalString = "";
+
+        for (int i = 0; i < 8; i++)  //create a random 8 digit string
+        {
+            finalString += chars[Mathf.RoundToInt(Random.Range(0, chars.Length))];
+        }
+        inputField.text = finalString;
+        inputField.onEndEdit.Invoke(finalString);
+        Debug.Log("Creating Session with the name: " + finalString);
+        foreach (Button button in buttons)
+        {
+            button.enabled = false;
+        }
+    }
+    public void Yell(string yellWhat)
+    {
+        Debug.Log("I'm Yelling, \"" + yellWhat + "\".");
+    }
+}
